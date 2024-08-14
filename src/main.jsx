61819f343f3e2374a2415f725a602ad79bfdf3c4
swati patel <124168505+SwatiP012@users.noreply.github.com>
2024-08-14 13:37:07 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+  
+export const Context = createContext({isAuthenticated: false, setisAuthenticated: () => {}, user: {}, setUser: () => {} });
+
+const AppWrapper = () => {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  const[user, setUser] = useState({});
+
+  return (
+    <Context.Provider value={{isAuthenticated, setisAuthenticated, user, setUser}}>
+      <App />
+    </Context.Provider>
+  );
+  
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>,
+)
